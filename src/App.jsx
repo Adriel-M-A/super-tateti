9f25e6ea09fc6eb95ff685e9dd8d3c0492af19ce
Adriel-M-A <adriel.dev97@gmail.com>
2026-03-05 13:15:45 -0300

    Inicio de Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-pink-500 drop-shadow-lg">
+        Super Ta-Te-Ti
+      </h1>
+    </div>
+  )
+}
+
+export default App
